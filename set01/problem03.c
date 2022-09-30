b74aeaae8b410b164e1a07f8da06d02fb76c0583
Chandana  B <114483230+chandanab200422@users.noreply.github.com>
2022-09-30 05:11:06 +0000

    solved

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-
+sum_calc(int x, int y, int *sum);
+  
 int main(void){
   int a, b, sum;
   printf("enter the first numbre\n");
   scanf("%d", &a);
-  printf("enter the seconf number\n");
+  printf("enter the second number\n");
   scarf("%d", &b);
- sum = a+b;
-  printf("%d",sum);
-  return 0;
-  }
+  sum_calc(x,y, &sum);
+  printf("sum is %d", sum);
+}
+
+sum_calc(int x, int y, int *sum)
+{
+  *sum=x+y;
+}
