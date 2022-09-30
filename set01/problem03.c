d95409112ce33a0297e37477ffddbf4260cf81a4
Chandana  B <114483230+chandanab200422@users.noreply.github.com>
2022-09-30 06:52:17 +0000

    solved

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-sum_calc(int x, int y, int *sum);
+sum_calc(int x,int y,int *sum);
   
 int main(void){
   int a, b, sum;
   printf("enter the first numbre\n");
   scanf("%d", &a);
   printf("enter the second number\n");
-  scarf("%d", &b);
-  sum_calc(x,y, &sum);
+  scanf("%d", &b);
+  sum_calc(a,b, &sum);
   printf("sum is %d", sum);
 }
+  
 
-sum_calc(int x, int y, int *sum)
+sum_calc(int a, int b, int *sum)
 {
-  *sum=x+y;
+  *sum=a+b;
 }
