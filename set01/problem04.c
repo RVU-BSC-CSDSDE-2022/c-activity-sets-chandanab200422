197688f32dde54d14e3fe3cf908995ad4eea9151
Chandana  B <114483230+chandanab200422@users.noreply.github.com>
2022-10-18 04:46:55 +0000

    s

--- set01/problem04.c
+++ set01/problem04.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
-void change(int *x, int *y);
-
+#include<stdio.h>
+void changa(int *x,int *y);
 int main(void){
-  int x,y;
-  x=8,x=7;
-  change(&x,&y);
+  int x,t;
+  x=8,y=7;
+  change($x,&y);
   printf("x is %d y is %d",x,y);
   return 0;
 }
-void change(int *x, int *y, int *sum)
+void change(int *x,int *y)
 {
-  *x=8;
-  *y=7;
-}    
+  *x = 22;
+  *y = 55;
+}
--- set01/problem05.c
+++ set01/problem05.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
+int input();
+int compare(int a, int b, int c);
+void output(int a,int b,int c,int largest);
 
-int main() {
-   int a, b, c;
-  printf("Enter the number a");
-  printf("Enter the number b");
-  printf("Entre the number c");
-
-  if ( a > b && a > c )
-      printf("%d is the largest.", a);
-   else if ( b > a && b > c )
-      printf("%d is the largest.", b);
-   else if ( c > a && c > b )
-      printf("%d is the largest.", c);
-   else   
-      printf("not equal");
-
-   return 0;
+int main(void){
+int a,b,c,l;
+a = input();
+b = input();
+c = input();
+l = compare(a,b,c);
+output(a,b,c,l);
 }
+
+int input(){
+  int x;
+  printf("enter a number\n");
+  scanf("%d",&x);
+  return(x);
+  }
+int compare(int a, int b , int c){
+  if(a>b){
+  {
+  else 
+  {
+    if(b>c){return(b);}
+    else{return (c);}
+    }
+  }
+void output(int a, int b, int c, int largest){
+  printf("the largest of %d, %d and %d is %d" a,b,c largest);
+}
+
+
+
+  
